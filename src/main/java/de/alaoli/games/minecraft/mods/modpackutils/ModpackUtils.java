3617e6f27b39b6444b933d52322c19cdc2bba11e
DerOli82 <DerOli82@alaoli.de>
2017-06-04 12:33:39 +0200

    initial commit

---
+++ src/main/java/de/alaoli/games/minecraft/mods/modpackutils/ModpackUtils.java
@@ -0,0 +1,12 @@
+package de.alaoli.games.minecraft.mods.modpackutils;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = ModpackUtils.MODID, version = ModpackUtils.VERSION)
+public class ModpackUtils
+{
+    public static final String MODID = "modpackutils";
+    public static final String VERSION = "0.1.0";
+    
+
+}
