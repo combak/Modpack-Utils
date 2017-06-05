48117ddd956a9139b0133eb2292471571877a51b
DerOli82 <DerOli82@alaoli.de>
2017-06-05 19:47:27 +0200

    Changelog config

--- src/main/java/de/alaoli/games/minecraft/mods/modpackutils/ModpackUtils.java
+++ src/main/java/de/alaoli/games/minecraft/mods/modpackutils/ModpackUtils.java
@@ -1,12 +1,49 @@
 package de.alaoli.games.minecraft.mods.modpackutils;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.StringJoiner;
+
+import de.alaoli.games.minecraft.mods.lib.common.Config;
+import de.alaoli.games.minecraft.mods.lib.common.Initialize;
+import de.alaoli.games.minecraft.mods.lib.common.data.DataException;
+import de.alaoli.games.minecraft.mods.modpackutils.config.ChangelogSection;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = ModpackUtils.MODID, version = ModpackUtils.VERSION)
-public class ModpackUtils
+public class ModpackUtils implements Initialize
 {
     public static final String MODID = "modpackutils";
     public static final String VERSION = "0.1.0";
     
-
+    /******************************************************************************************
+     * Method - Implements Initialize
+     ******************************************************************************************/
+    
+	@Override
+	public void preInit(FMLPreInitializationEvent event) throws IOException, DataException 
+	{
+		StringJoiner file  = new StringJoiner( File.separator)
+			.add( event.getModConfigurationDirectory().toString() )
+			.add( MODID + ".json" );
+		Config config = new Config();
+		config.registerSection( new ChangelogSection() );
+		config.setSavePath( file.toString() );
+		config.load();
+		config.cleanup();
+	}
+	
+	@Override
+	public void init(FMLInitializationEvent event) throws IOException, DataException {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void postInit(FMLPostInitializationEvent event) throws IOException, DataException {
+		// TODO Auto-generated method stub
+		
+	}
 }
---
+++ src/main/java/de/alaoli/games/minecraft/mods/modpackutils/config/ChangelogSection.java
@@ -0,0 +1,63 @@
+package de.alaoli.games.minecraft.mods.modpackutils.config;
+
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.JsonValue;
+
+import de.alaoli.games.minecraft.mods.lib.common.config.Section;
+import de.alaoli.games.minecraft.mods.lib.common.data.DataException;
+
+public class ChangelogSection implements Section 
+{
+    /******************************************************************************************
+     * Attribute
+     ******************************************************************************************/
+	
+	/**
+	 * Enables changelog view
+	 */
+	public static boolean enabled = true;
+	
+	/**
+	 * Changelog file name.
+	 */
+	public static String file = "CHANGELOG.md";
+	
+    /******************************************************************************************
+     * Method - Implements Section
+     ******************************************************************************************/
+	
+	@Override
+	public String getSectionName() 
+	{
+		return "Changelog";
+	}
+
+    /******************************************************************************************
+     * Method - Implements JsonSerializable
+     ******************************************************************************************/
+	
+	@Override
+	public JsonValue serialize() throws DataException { return null; /*Read only*/ }
+
+	@Override
+	public void deserialize( JsonValue json ) throws DataException 
+	{
+		if( !json.isObject() ) { throw new DataException( "ChangelogSection isn't a JsonObject." ); }
+		
+		JsonObject obj = json.asObject();
+		
+		if( obj.get( "enabled" ) != null )
+		{
+			if( !obj.get( "enabled" ).isBoolean() ) { throw new DataException( "ChangelogSection 'enabled' isn't an boolean." ); }
+			
+			enabled = obj.get( "enabled" ).asBoolean();
+		}
+		
+		if( obj.get( "file" ) != null )
+		{
+			if( !obj.get( "file" ).isString() ) { throw new DataException( "ChangelogSection 'file' isn't a string." ); }
+			
+			file = obj.get( "file" ).asString();
+		}
+	}
+}
