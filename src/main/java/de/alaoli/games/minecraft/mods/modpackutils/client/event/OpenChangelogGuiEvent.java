21e6ecb54835ab4e62c28cc730786f9e4cb0914d
DerOli82 <DerOli82@alaoli.de>
2017-06-12 12:45:15 +0200

    Implement changelog gui via chat command /mpu changelog

---
+++ src/main/java/de/alaoli/games/minecraft/mods/modpackutils/client/event/OpenChangelogGuiEvent.java
@@ -0,0 +1,8 @@
+package de.alaoli.games.minecraft.mods.modpackutils.client.event;
+
+import net.minecraftforge.fml.common.eventhandler.Event;
+
+public class OpenChangelogGuiEvent extends Event 
+{
+
+}
