22b38b2b258b0079956f98b5de1503ac66593b30
DerOli82 <DerOli82@alaoli.de>
2017-06-12 12:44:13 +0200

    Register commands & events

--- src/main/java/de/alaoli/games/minecraft/mods/modpackutils/ModpackUtils.java
+++ src/main/java/de/alaoli/games/minecraft/mods/modpackutils/ModpackUtils.java
@@ -6,20 +6,22 @@ import java.util.StringJoiner;
 
 import de.alaoli.games.minecraft.mods.lib.common.Config;
 import de.alaoli.games.minecraft.mods.lib.common.Initialize;
+import de.alaoli.games.minecraft.mods.lib.common.command.CommandGroup;
 import de.alaoli.games.minecraft.mods.lib.common.data.DataException;
-import de.alaoli.games.minecraft.mods.modpackutils.command.MPUCommandGroup;
-import de.alaoli.games.minecraft.mods.modpackutils.config.ChangelogSection;
-import de.alaoli.games.minecraft.mods.modpackutils.config.GithubWebserviceSection;
-import de.alaoli.games.minecraft.mods.modpackutils.config.WebservicesSection;
-import de.alaoli.games.minecraft.mods.modpackutils.event.handler.github.GithubEventHandler;
+import de.alaoli.games.minecraft.mods.modpackutils.client.command.BugReportCommand;
+import de.alaoli.games.minecraft.mods.modpackutils.client.command.ChangelogCommand;
+import de.alaoli.games.minecraft.mods.modpackutils.client.command.MPUCommandGroup;
+import de.alaoli.games.minecraft.mods.modpackutils.client.event.handler.ChangelogEventHandler;
+import de.alaoli.games.minecraft.mods.modpackutils.client.event.handler.github.GithubEventHandler;
+import de.alaoli.games.minecraft.mods.modpackutils.common.config.ChangelogSection;
+import de.alaoli.games.minecraft.mods.modpackutils.common.config.GithubWebserviceSection;
+import de.alaoli.games.minecraft.mods.modpackutils.common.config.WebservicesSection;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
 @Mod( modid = ModpackUtils.MODID, version = ModpackUtils.VERSION, clientSideOnly = true )
 public class ModpackUtils implements Initialize
@@ -55,7 +57,21 @@ public class ModpackUtils implements Initialize
     @EventHandler 
 	public void init( FMLInitializationEvent event ) throws IOException, DataException 
 	{
-		ClientCommandHandler.instance.registerCommand( new MPUCommandGroup() );
+		CommandGroup commands = new MPUCommandGroup();
+		
+		if( ChangelogSection.enabled )
+		{
+			ChangelogEventHandler.register();
+			commands.add( new ChangelogCommand( commands ) );
+		}
+		
+		if( WebservicesSection.enabled && GithubWebserviceSection.enabled )
+		{
+			GithubEventHandler.register();
+			commands.add( new BugReportCommand( commands ) );
+			
+		}
+		ClientCommandHandler.instance.registerCommand( commands );
 	}
 	
 	@Override
