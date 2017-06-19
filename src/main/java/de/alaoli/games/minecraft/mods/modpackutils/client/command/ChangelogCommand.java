31b84f11a32285c74b3dcc1fa35c0a2e4fdd3791
DerOli82 <DerOli82@alaoli.de>
2017-06-19 17:37:04 +0200

    Cleanup

--- src/main/java/de/alaoli/games/minecraft/mods/modpackutils/client/command/ChangelogCommand.java
+++ src/main/java/de/alaoli/games/minecraft/mods/modpackutils/client/command/ChangelogCommand.java
@@ -3,7 +3,6 @@ package de.alaoli.games.minecraft.mods.modpackutils.client.command;
 import de.alaoli.games.minecraft.mods.lib.common.command.Arguments;
 import de.alaoli.games.minecraft.mods.lib.common.command.Command;
 import de.alaoli.games.minecraft.mods.modpackutils.client.event.OpenChangelogGuiEvent;
-import jline.internal.Log;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.common.MinecraftForge;
@@ -39,34 +38,7 @@ public class ChangelogCommand extends Command
 	@Override
 	public void processCommand( Arguments args )
 	{
-		Log.info( "bla" );
 		MinecraftForge.EVENT_BUS.post( new OpenChangelogGuiEvent() );
-		/*
-		
-		
-		if( file.exists() )
-		{
-			try 
-			{
-				String line;
-				BufferedReader reader = new BufferedReader( new FileReader( file ) );
-				
-				while( ( line = reader.readLine() ) != null  )
-				{
-					args.sender.addChatMessage( new TextComponentString( line ));
-				}
-				reader.close();
-			}
-			catch( IOException e ) 
-			{
-				throw new CommandException( "Changelog not found.", e );
-			}
-		}
-		else
-		{
-			args.sender.addChatMessage( new TextComponentString( "No changelog found." ));
-		}
-		*/
 	}
 
 }
