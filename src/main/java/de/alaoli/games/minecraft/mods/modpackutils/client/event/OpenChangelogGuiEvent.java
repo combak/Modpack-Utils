d8cc33604942530f77349e6e1a9cc6ae63ec3b90
DerOli82 <DerOli82@alaoli.de>
2017-10-18 19:27:43 +0200

    Implement OpenScreenEvent class and some event renaming

--- src/main/java/de/alaoli/games/minecraft/mods/modpackutils/client/event/OpenChangelogGuiEvent.java
+++
@@ -1,8 +0,0 @@
-package de.alaoli.games.minecraft.mods.modpackutils.client.event;
-
-import net.minecraftforge.fml.common.eventhandler.Event;
-
-public class OpenChangelogGuiEvent extends Event 
-{
-
-}
---
+++ src/main/java/de/alaoli/games/minecraft/mods/modpackutils/client/event/OpenScreenEvent.java
@@ -0,0 +1,15 @@
+package de.alaoli.games.minecraft.mods.modpackutils.client.event;
+
+import de.alaoli.games.minecraft.mods.lib.common.ui.Screen;
+import net.minecraftforge.fml.common.eventhandler.Event;
+
+public class OpenScreenEvent extends Event 
+{
+	public final Screen screen;
+	
+	public OpenScreenEvent( Screen screen )
+	{
+		super();
+		this.screen = screen;
+	}
+}
