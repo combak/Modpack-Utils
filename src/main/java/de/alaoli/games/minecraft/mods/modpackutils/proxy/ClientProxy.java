83b8112ff82323d477a0f94f6669c1fb1e6bddc1
DerOli82 <DerOli82@alaoli.de>
2017-10-18 19:35:24 +0200

    Implement CommonProxy, maybe in the future not only client side

--- src/main/java/de/alaoli/games/minecraft/mods/modpackutils/proxy/ClientProxy.java
+++ src/main/java/de/alaoli/games/minecraft/mods/modpackutils/proxy/ClientProxy.java
@@ -2,30 +2,27 @@ package de.alaoli.games.minecraft.mods.modpackutils.proxy;
 
 import java.io.IOException;
 
-import de.alaoli.games.minecraft.mods.lib.common.Initialize;
 import de.alaoli.games.minecraft.mods.lib.common.command.CommandGroup;
 import de.alaoli.games.minecraft.mods.lib.common.data.DataException;
 import de.alaoli.games.minecraft.mods.modpackutils.client.command.BugReportCommand;
 import de.alaoli.games.minecraft.mods.modpackutils.client.command.ChangelogCommand;
 import de.alaoli.games.minecraft.mods.modpackutils.client.command.MPUCommandGroup;
-import de.alaoli.games.minecraft.mods.modpackutils.client.event.handler.ChangelogEventHandler;
-import de.alaoli.games.minecraft.mods.modpackutils.client.event.handler.github.GithubEventHandler;
+import de.alaoli.games.minecraft.mods.modpackutils.client.event.handler.ScreenEventHandler;
+import de.alaoli.games.minecraft.mods.modpackutils.client.event.handler.webservices.GithubEventHandler;
+import de.alaoli.games.minecraft.mods.modpackutils.client.integration.custommainmenu.event.handler.CustomMainMenuEventHandler;
 import de.alaoli.games.minecraft.mods.modpackutils.common.config.ChangelogSection;
 import de.alaoli.games.minecraft.mods.modpackutils.common.config.WebservicesSection;
+import de.alaoli.games.minecraft.mods.modpackutils.common.config.integration.CustomMainMenuSection;
 import de.alaoli.games.minecraft.mods.modpackutils.common.config.webservices.GithubSection;
 import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-public class ClientProxy implements Initialize 
+public class ClientProxy extends CommonProxy
 {
     /******************************************************************************************
-     * Method - Implements Initialize
+     * Method - Implement CommonProxy
      ******************************************************************************************/
-	
-	@Override
-	public void preInit( FMLPreInitializationEvent event ) throws IOException, DataException {}
 
 	@Override
 	public void init( FMLInitializationEvent event ) throws IOException, DataException 
@@ -34,21 +31,17 @@ public class ClientProxy implements Initialize
 		
 		if( ChangelogSection.enabled )
 		{
-			ChangelogEventHandler.register();
-			commands.addNode( new ChangelogCommand( commands ) );
+			commands.addComponent( new ChangelogCommand() );
 		}
 		
 		if( WebservicesSection.enabled && GithubSection.enabled )
 		{
 			GithubEventHandler.register();
-			commands.addNode( new BugReportCommand( commands ) );
+			commands.addComponent( new BugReportCommand() );
 			
 		}
 		ClientCommandHandler.instance.registerCommand( commands );
-		
+	
+		ScreenEventHandler.register();
 	}
-
-	@Override
-	public void postInit(FMLPostInitializationEvent event) throws IOException, DataException {}
-
 }
--- src/main/java/de/alaoli/games/minecraft/mods/modpackutils/proxy/ServerProxy.java
+++ src/main/java/de/alaoli/games/minecraft/mods/modpackutils/proxy/ServerProxy.java
@@ -1,26 +1,6 @@
 package de.alaoli.games.minecraft.mods.modpackutils.proxy;
 
-import java.io.IOException;
-
-import de.alaoli.games.minecraft.mods.lib.common.Initialize;
-import de.alaoli.games.minecraft.mods.lib.common.data.DataException;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-
-public class ServerProxy implements Initialize 
+public class ServerProxy extends CommonProxy
 {
-    /******************************************************************************************
-     * Method - Implements Initialize
-     ******************************************************************************************/
-	
-	@Override
-	public void preInit( FMLPreInitializationEvent event ) throws IOException, DataException {}
-
-	@Override
-	public void init( FMLInitializationEvent event ) throws IOException, DataException {}
-
-	@Override
-	public void postInit(FMLPostInitializationEvent event) throws IOException, DataException {}
 
 }
